c78ffbf1db97a2dcd8d8db0340d8d46c275746ff
me-chetna <chetna.jain.ug24@nsut.ac.in>
2025-10-18 13:08:36 +0530

    adD

---
+++ index.js
@@ -0,0 +1,16 @@
+function greet(userName) {
+    event.preventDefault();
+    document.getElementById("title").innerHTML = `Hello, ${userName}`;
+}
+function changecolor(boxID){
+    const box =document.getElementById(boxID);
+    if( boxID === "box1"){
+        box.style.backgroundColor="red";
+    }else if(boxID === "box2"){
+        box.style.backgroundColor="green";
+    }else if(boxID === "box3"){
+        box.style.backgroundColor="blue";
+    }else if(boxID === "box4"){
+        box.style.backgroundColor="yellow";
+    }
+}
